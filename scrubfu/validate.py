9ad8d1456bc0929edc3010436f093f65fb18a3fa
yusufigoga <yusufigoga@gmail.com>
2023-11-17 11:26:02 +0200

    added logging

--- scrubfu/validate.py
+++ scrubfu/validate.py
@@ -1,31 +1,49 @@
+
 import re
+import sys
+import logging
 
 class ColumnCommentValidator:
     def __init__(self, comment):
         self.comment = comment
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.DEBUG)
+
+        #Standard output
+        stdout_handler = logging.StreamHandler(sys.stdout)
+        stdout_handler.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        stdout_handler.setFormatter(formatter)
+        self.logger.addHandler(stdout_handler)
+
+        #Standard error
+        stderr_handler = logging.StreamHandler(sys.stderr)
+        stderr_handler.setLevel(logging.ERROR)
+        stderr_handler.setFormatter(formatter)
+        self.logger.addHandler(stderr_handler)
 
     def validate(self):
         # Check if the comment starts and ends with tildes (~)
         if not self.comment.startswith("~") or not self.comment.endswith("~"):
-            print("Invalid comment format. It should be enclosed in tildes (~).")
+            self.logger.error("Invalid comment format. It should be enclosed in tildes (~).")
             return False
 
         # Extract the content between tildes for validation
         comment_content = self.comment[1:-1]
 
-        # Check if multiple script-tag are present and follow the specified format
+        # Check if multiple script-tags are present and follow the specified format
         script_tags = comment_content.split('~')
         for script in script_tags:
             if script.strip() and not self.is_valid_script_tag(script.strip()):
-                print("Invalid script-tag format: {script}")
+                self.logger.error(f"Invalid script-tag format: {script}")
                 return False
 
-        print("Comment is valid.")
+        self.logger.info("Comment is valid.")
         return True
 
     def is_valid_script_tag(self, script):
         # Validate the format and parameters of each script-tag
-        script_pattern = re.compile(r'^([a-zA-Z]+:)?(MASK|REPLACE|RANDOM|LIST|DROP|MA|RE|RA|LI|DR):[a-zA-Z0-9_]+((\d+))?$')
+        script_pattern = re.compile(r'^([a-zA-Z]+:)?(MASK|REPLACE|RANDOM|LIST|DROP|MA|RE|RA|LI|DR):[a-zA-Z0-9]+((\d+))?$')
         if not script_pattern.match(script):
             return False
 
@@ -41,9 +59,9 @@ class ColumnCommentValidator:
         valid_tags = {'MASK', 'REPLACE', 'RANDOM', 'LIST', 'DROP', 'MA', 'RE', 'RA', 'LI', 'DR'}
         if tag not in valid_tags:
             return True
-        
+
 
 #Example for testing:
-comment = "~MASK:email ~REPLACE:12345 ~RANDOM:10~"
+comment = "~MASK:email~"
 validator = ColumnCommentValidator(comment)
 validator.validate()
