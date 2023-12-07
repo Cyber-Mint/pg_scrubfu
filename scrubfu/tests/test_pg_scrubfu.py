dc0d98a0accdf189b0e55c81b58fce676b89e462
ANVanR <alex.vanrijswijck@gmail.com>
2023-12-07 15:41:57 +0200

    fixed import

--- scrubfu/tests/test_pg_scrubfu.py
+++ scrubfu/tests/test_pg_scrubfu.py
@@ -1,14 +1,13 @@
 from unittest import TestCase
-from scrubfu.pg_scrubfu import main
+from scrubfu.pg_scrubfu import cli
 from io import StringIO
 from contextlib import redirect_stdout
 
 
-class Test_pg_scrubfu(TestCase):
+class TestPGScrubfu(TestCase):
     def test_pg_scrubfu(self):
         f = StringIO()
-
         with redirect_stdout(f):
-            main()
+            cli()
 
         self.assertTrue(isinstance(f.getvalue(), str))
